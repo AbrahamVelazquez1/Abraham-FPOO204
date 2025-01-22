8c024e1f86a2f1320319511428695545aa1e3da2
AbrahamVelazquez1 <122043950@upq.edu.mx>
2025-01-22 15:36:41 -0600

    Se agregaron nuevas funciones
    Se implementaron nuevas funciones que hacen distintos metodos de comparacion

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hola soy Abraham!");
         String cadena = "Hola" + "Soy" + "Abraham";
 
@@ -9,5 +13,39 @@ public class Main {
 
         System.out.println(cadena.substring(0, 1)); //Extraer los datos de la cadena en A a B posicion
 
+        int a = 1, a2, a3 = 500, a4 = 20;
+        double b = 45.5, b2;
+        String c = "bskjog";
+        String d = "1";
+        String f; //guarda el valor de a3 en modo cadena
+
+        a2 = Integer.parseInt(d);
+        f = String.valueOf(a3);
+        b2 = Double.valueOf(a4);
+
+        Random numeroRandom = new Random();
+
+        int random_entero = numeroRandom.nextInt();
+        double random_double = numeroRandom.nextDouble();
+
+        System.out.println(random_entero);
+        System.out.println(random_double);
+
+        System.out.println("Ingresa algo: ");
+        String dato = sc.nextLine();
+
+        System.out.println("Usted ingreso: " + dato);
+
+        System.out.println(10 > 9);
+        System.out.println(10 < 9);
+        System.out.println(10 >= 9);
+        System.out.println(10 <= 9);
+        System.out.println(10 != 9);
+
+        int x=3;
+
+        System.out.println(x<5 && x>10);
+        System.out.println(x<5 || x>10);
+        
     }
 }
