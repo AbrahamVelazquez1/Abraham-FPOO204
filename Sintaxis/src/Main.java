b300eba09b5744282f5799b21e7514f56944eb38
AbrahamVelazquez1 <122043950@upq.edu.mx>
2025-01-22 14:42:45 -0600

    Funciones para cadena
    Se agregaron una serie de funciones para exraer caracteres dentro de la cadena y obtener la longitud de la misma

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola soy Abraham!");
+        String cadena = "Hola" + "Soy" + "Abraham";
+
+        System.out.println(cadena);
+
+        System.out.println(cadena.length()); //Obtener la longitud de la cadena
+
+        System.out.println(cadena.substring(0, 1)); //Extraer los datos de la cadena en A a B posicion
+
     }
 }
